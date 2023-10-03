c56ea7dd58ce54f199f1a4d1640e43550a440d11
MFaisalZ <40825206+MFaisalZ@users.noreply.github.com>
2023-10-03 22:29:43 +0700

    Add files via upload

---
+++ faisal.js
@@ -0,0 +1,8 @@
+var firstName = "Muhammad Faisal"
+var lastName = "Zulmaulidin" 
+var age = 24;
+var isMarried = false;
+
+console.log("Nama Lengkap : " + firstName + " " +lastName)
+console.log("Umur  : " + age)
+console.log("Status Pernikahan : " + isMarried)
