2657750ad77aa0635f862885da876a6240d7638b
MFaisalZ <40825206+MFaisalZ@users.noreply.github.com>
2023-10-16 22:54:31 +0700

    Add files via upload

---
+++ tugas3.js
@@ -0,0 +1,32 @@
+/*
+Nama 	: Muhammad Faisal Zulmaulidin
+NIM 	: 20220040171
+Kelas	: TI 22 M
+Tugas 	: Struktur Data - sesi 3 array object 
+/variabel restaurant
+*/
+const restaurant = {
+	name: "Mie SP",
+	city: "Sukabumi",
+	favorietDrink: "Es Teh Manis",
+	favvoriteFood: "Mie Goreng SP Lev 5",
+	isVegan: true // atau false tergantung
+};
+
+const restoranSmi = [
+	{
+		name: "Mie Gacoan",
+		city: "Bogor",
+		favorietDrink: "Es Teh Manis",
+		favvoriteFood: "Mie Gacoan Level 5",
+		isVegan: true // atau false tergantung
+	},
+	{
+		name: "Kabumi",
+		city: "Sukabumi",
+		favorietDrink: "Coffe Arabica",
+		favvoriteFood: "Nasi Goreng Seafood",
+		isVegan: true // atau false tergantung
+	}
+];
+
